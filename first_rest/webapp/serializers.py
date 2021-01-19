dfa2a18c482037fc6b5a85e301143a47f0d448a8
Jaimin-dev21 <prajapatijaimin1021@gmail.com>
2021-01-18 23:48:55 -0500

    my commit to repo

---
+++ first_rest/webapp/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from . models import Employees
+
+class EmployeesSerializers(serializers.ModelSerializer):
+
+	class Meta:
+		model = Employees
+		# fields = ('first_name', 'last_name')
+		fields = '__all__'
